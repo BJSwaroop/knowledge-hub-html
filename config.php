c4d224642d2e8be6f2887cbd0d32b01270f779b0
B Jyothi Swaroop <72175383+BJSwaroop@users.noreply.github.com>
2021-08-15 15:03:19 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 This file contains database configuration assuming
 */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'id17422540_db_knowledge');
-define('DB_PASSWORD', 'N=d*avknMp86^wCc');
-define('DB_NAME', 'id17422540_knowledge_hub');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'login');
 
 
 $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
